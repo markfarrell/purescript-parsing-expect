b8069c448ad1467865bc83b7e22e1fc07dbeaa7b
Mark Farrell <markfarr2011@gmail.com>
2020-07-17 02:29:12 +0000

    compatibility

--- src/Text/Parsing/Expect.purs
+++ src/Text/Parsing/Expect.purs
@@ -6,39 +6,20 @@ module Text.Parsing.Expect
 
 import Prelude
 
-import Data.Either as E
-import Data.Traversable (class Traversable, sequence)
-
 import Effect (Effect)
-import Effect.Exception (throw) as Exception
 
-import Text.Parsing.Parser (ParserT, runParserT)
+import Text.Parsing.Parser (Parser)
 
--- Helper function for `success`, `failure`, and `expect`.
-assertEqual :: forall a. Eq a => a -> a -> Effect Unit
-assertEqual x y = case x == y of
-  false -> Exception.throw $ "Assertion failure (==)."
-  true  -> pure unit
-
--- Helper function for `success` and `failure`.
-successful :: forall a m b. Monad m => Traversable m => Boolean -> a -> ParserT a m b -> Effect Unit
-successful w x y = do
-  z  <- pure (E.isRight $ sequence $ runParserT x y)
-  _  <- assertEqual z w
-  pure unit
+import Text.Parsing.Parser.Expect as E
 
 -- | Asserts that the result of running a parser `p` with input `x` was successful.
-success :: forall a m b. Monad m => Traversable m => a -> ParserT a m b -> Effect Unit
-success = successful true
+success :: forall a b. a -> Parser a b -> Effect Unit
+success = E.success
 
 -- | Asserts that the result of running a parser `p` with input `x` was unsuccessful.
-failure :: forall a m b. Monad m => Traversable m => a -> ParserT a m b -> Effect Unit
-failure = successful false
+failure :: forall a b. a -> Parser a b -> Effect Unit
+failure = E.failure
 
 -- | Asserts that the result of running a parser `p` with input `x` is `w`.
-output :: forall a m b. Monad m => Traversable m => Eq b => b -> a -> ParserT a m b -> Effect Unit
-output w x = \p -> do
-  y <- pure $ sequence $ runParserT x p
-  case y of
-    (E.Left _)  -> Exception.throw $ "Assertion failure (output)."
-    (E.Right z) -> void $ sequence (assertEqual w <$> z)
+output :: forall a b. Eq b => b -> a -> Parser a b -> Effect Unit
+output = E.output
