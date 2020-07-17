296ddb96a79c6a1acbbb8a4260066286ab046022
Mark Farrell <markfarr2011@gmail.com>
2020-07-17 01:29:29 +0000

    success,failure,output

--- src/Text/Parsing/Expect.purs
+++ src/Text/Parsing/Expect.purs
@@ -7,11 +7,12 @@ module Text.Parsing.Expect
 import Prelude
 
 import Data.Either as E
+import Data.Traversable (class Traversable, sequence)
 
 import Effect (Effect)
 import Effect.Exception (throw) as Exception
 
-import Text.Parsing.Parser (Parser, runParser)
+import Text.Parsing.Parser (ParserT, runParserT)
 
 -- Helper function for `success`, `failure`, and `expect`.
 assertEqual :: forall a. Eq a => a -> a -> Effect Unit
@@ -20,24 +21,24 @@ assertEqual x y = case x == y of
   true  -> pure unit
 
 -- Helper function for `success` and `failure`.
-successful :: forall a b. Boolean -> a -> Parser a b -> Effect Unit
+successful :: forall a m b. Monad m => Traversable m => Boolean -> a -> ParserT a m b -> Effect Unit
 successful w x y = do
-  z  <- pure (E.isRight $ runParser x y)
+  z  <- pure (E.isRight $ sequence $ runParserT x y)
   _  <- assertEqual z w
   pure unit
 
 -- | Asserts that the result of running a parser `p` with input `x` was successful.
-success :: forall a b. a -> Parser a b -> Effect Unit
+success :: forall a m b. Monad m => Traversable m => a -> ParserT a m b -> Effect Unit
 success = successful true
 
 -- | Asserts that the result of running a parser `p` with input `x` was unsuccessful.
-failure :: forall a b. a -> Parser a b -> Effect Unit
+failure :: forall a m b. Monad m => Traversable m => a -> ParserT a m b -> Effect Unit
 failure = successful false
 
 -- | Asserts that the result of running a parser `p` with input `x` is `w`.
-output :: forall a b. Eq b => b -> a -> Parser a b -> Effect Unit
+output :: forall a m b. Monad m => Traversable m => Eq b => b -> a -> ParserT a m b -> Effect Unit
 output w x = \p -> do
-  y <- pure $ runParser x p
+  y <- pure $ sequence $ runParserT x p
   case y of
     (E.Left _)  -> Exception.throw $ "Assertion failure (output)."
-    (E.Right z) -> assertEqual z w 
+    (E.Right z) -> void $ sequence (assertEqual w <$> z)
